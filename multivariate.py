62fd8632850346fca5d69ce36f6ec133dd9a5c00
Cameron Hartmann <camhart73@gmail.com>
2014-10-08 02:50:25 -0600

    as

--- multivariate.py
+++ multivariate.py
@@ -1,4 +1,4 @@
-import signal
+import signal, math
 from base import Base
 
 class MultiVariateLinearRegression(Base):
@@ -10,7 +10,7 @@ class MultiVariateLinearRegression(Base):
         self.w = [5]
         self.alpha = 0.000001
         self.filename = 'multivariate_data-train.csv'
-        self.convergenceCondition = 1e-6
+        self.convergenceCondition = 1e-14
         self.ysum = None
 
     def loadCSVFile(self, filename=None):
@@ -26,26 +26,35 @@ class MultiVariateLinearRegression(Base):
         nlist = [self.x[j][index] * (self.y[j]-self.h(j)) for j in xrange(len(self.y))]
         return sum(nlist)
 
+    def MSE(self):
+        '''
+        calculate the mean square error
+        '''
+        nlist = [ math.pow(self.y[i] - self.h(i), 2) for i in xrange(len(self.y))]
+        self.msex.append(self.cc)
+        self.msey.append(sum(nlist))
+
     def findLine(self):
         ''' Find the line given the points loaded from csv file.
         '''
+        self.msex = []  #iteration count (x var on plot)
+        self.msey = []  #mse (y variable on plot)
+
         self.w = self.w * len(self.x[0])
         hasConverged = False
-        cc = 0
+        self.cc = 0
         while(hasConverged == False):    #convergence test
             hasConverged = True
-#         while(cc < 1000000):
             for i in xrange(len(self.w)):
                 curW = self.w[i] + (self.alpha * self.summ(i))
                 if(abs(curW - self.w[i]) > self.convergenceCondition):
                     hasConverged = False
                 self.w[i] = curW
-
-            if(cc % 100000 == 0):
-                print self.w
-                print cc
-            cc+=1
+            if(self.cc % 10000 == 0):
+                self.MSE()
+            self.cc+=1
         print 'total iterations:',cc
+        print 'mse:',self.mse
         return self.w
 
     @classmethod
@@ -54,24 +63,23 @@ class MultiVariateLinearRegression(Base):
             mvlr = MultiVariateLinearRegression()
         mvlr.loadCSVFile('multivariate_data-train.csv')
 #         mvlr.loadCSVFile('univariate_data-train.csv')
-#         mvlr.loadCSVFile('test1.csv')
-#         mvlr.loadCSVFile('test.csv')
-#         mvlr.printValues()
-#         ws = [-1]
         ws = mvlr.findLine()
         return ws
 
-smvlr = None
-
-def handler(signum, frame):
-    print smvlr.w
-
 def main():
     smvlr = MultiVariateLinearRegression()
+
+    def handler(signum, frame):
+        print smvlr.w
+        print 'iterations:',smvlr.cc
+        print 'msex', smvlr.msex
+        print 'msey', smvlr.msey
+
+    print 'type ctrl+break or ctrl+pause to print current w and iteration count'
+    # register ctrl+break to print values
+    signal.signal(signal.SIGBREAK, handler)  #windows only
+
     print MultiVariateLinearRegression.run(smvlr)
     print 'done'
-
-signal.signal(signal.SIGINT, handler)  #windows only
-
 
 main()
